3dd5a478dcdf838491258a4d62e95b7c1444d363
bleutzinn <bleutzinn@rwgc.nl>
2017-01-31 19:35:20 +0100

    Removed dependency of spyc.php class for YAML handling

--- add-page-by-form.php
+++ add-page-by-form.php
@@ -3,11 +3,12 @@ namespace Grav\Plugin;
 
 use Grav\Common\Grav;
 use Grav\Common\Plugin;
-use Grav\Common\Page\Page;
 use Grav\Common\Uri;
 use RocketTheme\Toolbox\Event\Event;
-
-require_once "vendor/spyc.php";
+//use Grav\Common\Filesystem\Folder;
+use Grav\Common\Page\Page;
+use RocketTheme\Toolbox\File\File;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class AddPageByFormPlugin
@@ -31,136 +32,6 @@ class AddPageByFormPlugin extends Plugin
             'onPluginsInitialized' => ['onPluginsInitialized', 0],
             'onFormProcessed' => ['onFormProcessed', 0]
         ];
-    }
-
-
-    public function onFormProcessed(Event $event)
-    {
-        $form = $event['form'];
-        $action = $event['action'];
-        $params = $event['params'];
-    
-        switch ($action) {
-            case 'addpage':
-                //do what you want
-                if(isset($_POST)) {
-                    // Get plugin config settings
-                    //$newPageRoute = $this->config->get('plugins.add-page-by-form.route');
-                    //$newPageTemplate = $this->config->get('plugins.add-page-by-form.template');
-                    $dateFormat = $this->config->get('plugins.add-page-by-form.dateformat');
-
-                    // Get all form fields
-                    $formdata = $form->value()->toArray();
-                    // Extract the content; if not present as a form value then fallback to frontmatter
-                    $content = 'No content set';
-                    if (isset($formdata['content']) ) {
-                        $content = $formdata['content'];
-                    }
-                    else {
-                        if (isset($header->pagefrontmatter->content) ) {
-                            $content = $header->pagefrontmatter->content;
-                        }
-                    }
-
-                    //$this->grav['debugger']->addMessage('Content: ' . $content );
-
-                    // Assemble the new page frontmatter from the pagefrontmatter block as set in
-                    // the form page and the form field values. Field values override values set
-                    // in the pagefrontmatter block
-                    $page = $this->grav['page'];
-                    $header = $page->header();
-                    $yaml_str = '';
-                    if ( isset($header->pagefrontmatter) && is_array($header->pagefrontmatter) ) {
-                        $pagefrontmatter = $header->pagefrontmatter;
-                        $formdata = $form->value()->toArray();
-                        if (isset($formdata)) {
-                            $pagefrontmatter = array_merge($pagefrontmatter, $form->value()->toArray());
-                        }
-                        // Remove content from array
-                        unset($pagefrontmatter['content']);
-                        // Convert array to a YAML formatted string
-                        $yaml_str = spyc_dump($pagefrontmatter);
-                        //dump($yaml_str);
-                        //$this->grav['debugger']->addMessage('The \'pagefrontmatter\' block is: ' . $yaml_str );
-                    }
-
-                    // Create s slug to be used as the page filename
-                    // Credits: Alex Garrett
-                    $slug = $pagefrontmatter['title'];
-                    $lettersNumbersSpacesHyphens = '/[^\-\s\pN\pL]+/u';
-                    $spacesDuplicateHypens = '/[\-\s]+/';
-                    $slug = preg_replace($lettersNumbersSpacesHyphens, '', $slug);
-                    $slug = preg_replace($spacesDuplicateHypens, '-', $slug);
-                    $slug = trim($slug, '-');
-                    $slug = mb_strtolower($slug, 'UTF-8');
-
-                    if ( isset($header->parent) ) {
-                        $parent_page = $this->grav['page']->find($header->parent);
-                        // Check whether the parent page exists
-                        if (!$parent_page) {
-                            throw new \Exception('Unable to add page; the parent "'.$header->parent.'" does not exist');
-                        }
-                    }
-                    else {
-                        throw new \Exception('Missing "parent" variable in form page header');
-                    }
-
-                    $newPageDir = $parent_page->path() . '/' . $slug;
-
-                    // Assume this is the first submission of the page, so set $version to 1
-                    $version = 0;
-
-                    // Keep incrementing the page slug suffix to keep earlier versions / duplicates
-                    while (file_exists($newPageDir)) {
-                        $version += 1;
-                        $newPageDir = $parent_page->path() . '/' . $slug . '-' . $version;
-                    }
-
-                    // Add the page
-                    try {
-                        // Create the directory
-                        $pageDir = mkdir($newPageDir, 0755, true);
-                        if (!$pageDir) {
-                            throw new \Exception('Unable to add page; can not create directory "' . $newPageDir . '"');
-                        }
-                        // Create the page file
-                        $pageFile = fopen($newPageDir . '/default.md', "w");
-                        // test case $pageFile = false;
-                        if (!$pageFile) {
-                            throw new \Exception('Unable to add page; can not create file "default.md"');
-                        }
-                        // Include the page frontmatter
-                        $txt = "---\n";
-                        fwrite($pageFile, $txt);
-                        // Insert the timestamp
-                        $txt = "date: " . date($dateFormat) . "\n";
-                        fwrite($pageFile, $txt);
-                        // Insert all frontmatter
-                        if ($yaml_str != '') {
-                            $txt = $yaml_str;
-                            fwrite($pageFile, $txt);
-                        }
-                        $txt = "---\n";
-                        fwrite($pageFile, $txt);
-
-                        // Include the page content
-                        $txt = $content . "\n";
-                        fwrite($pageFile, $txt);
-
-                        // Close and save the file
-                        fclose($pageFile);
-                    }
-                    catch (\Exception $e) {
-                        $this->grav['debugger']->addMessage($e->getMessage());
-                        $this->grav->fireEvent('onFormValidationError', new Event([
-                            'form'    => $form,
-                            'message' => '<strong>ERROR:</strong> ' . $e->getMessage() ]));
-                        $event->stopPropagation();
-                        return;
-                    }
-    
-                }
-        }
     }
 
     /**
@@ -194,6 +65,114 @@ class AddPageByFormPlugin extends Plugin
         $assets->addInlineJs(file_get_contents('plugin://add-page-by-form/assets/js/simplemde_config.js'), 1);
     }
 
+    public function onFormProcessed(Event $event)
+    {
+        $form = $event['form'];
+        $action = $event['action'];
+        $params = $event['params'];
+    
+        switch ($action) {
+            case 'addpage':
+                if(isset($_POST)) {
+                    // Get plugin config settings
+                    $dateFormat = $this->config->get('plugins.add-page-by-form.dateformat');
+
+                    // Get all form fields
+                    $formdata = $form->value()->toArray();
+                    // Extract the content; if not present as a form value then fallback to frontmatter
+                    $content = 'No content set';
+                    if (isset($formdata['content']) ) {
+                        $content = $formdata['content'];
+                    }
+                    else {
+                        if (isset($header->pagefrontmatter->content) ) {
+                            $content = $header->pagefrontmatter->content;
+                        }
+                    }
+                    // Assemble the new page frontmatter from the pagefrontmatter block as set in
+                    // the form page and the form field values. Field values override values set
+                    // in the pagefrontmatter block
+                    $page = $this->grav['page'];
+                    $header = $page->header();
+                    $yaml_str = '';
+                    if ( isset($header->pagefrontmatter) && is_array($header->pagefrontmatter) ) {
+                        $pagefrontmatter = $header->pagefrontmatter;
+                        $formdata = $form->value()->toArray();
+                        if (isset($formdata)) {
+                            $pagefrontmatter = array_merge($pagefrontmatter, $form->value()->toArray());
+                        }
+                        // Remove content from array
+                        unset($pagefrontmatter['content']);
+                    }
+
+                    // Create s slug to be used as the page filename
+                    // Credits: Alex Garrett
+                    $slug = $pagefrontmatter['title'];
+                    $lettersNumbersSpacesHyphens = '/[^\-\s\pN\pL]+/u';
+                    $spacesDuplicateHypens = '/[\-\s]+/';
+                    $slug = preg_replace($lettersNumbersSpacesHyphens, '', $slug);
+                    $slug = preg_replace($spacesDuplicateHypens, '-', $slug);
+                    $slug = trim($slug, '-');
+                    $slug = mb_strtolower($slug, 'UTF-8');
+
+                    if ( isset($header->parent) ) {
+                        $parent_page = $this->grav['page']->find($header->parent);
+                        // Check whether the parent page exists
+                        if (!$parent_page) {
+                            throw new \Exception('Unable to add page; the parent "'.$header->parent.'" does not exist');
+                        }
+                    }
+                    else {
+                        throw new \Exception('Missing "parent" variable in form page header');
+                    }
+
+                    $newPageDir = $parent_page->path() . '/' . $slug;
+                    // Assume this is the first submission of the page, so set $version to 1
+                    $version = 0;
+                    // Keep incrementing the page slug suffix to keep earlier versions / duplicates
+                    while (file_exists($newPageDir)) {
+                        $version += 1;
+                        $newPageDir = $parent_page->path() . '/' . $slug . '-' . $version;
+
+                    }
+                    if ($version > 0) {
+                        $slug = $slug . '-' . $version;
+                    }
+
+                    // Add the page
+                    try {
+                        /** @var Pages $pages */
+                        $pages = $this->grav['pages'];
+                        // Create page.
+                        $page = new Page;
+                        $language = trim(basename($parent_page->extension(), 'md'), '.') ?: null;
+                        if ($language != '') {
+                          $page->name('default.' . $language . '.md');
+                        }
+                        else {
+                            $page->name('default.md');
+                        }
+                        $path = $parent_page->path() . DS . $slug. DS . $page->name();
+                        $page->filePath($path);
+                        $header = $pagefrontmatter;
+                        $page->header((object) $header);
+                        $page->frontmatter(Yaml::dump((array) $page->header(), 10, 2, false));
+                        $page->rawMarkdown((string) $content);
+                        $page->content((string) $content);
+                        $page->file()->markdown($page->rawMarkdown());
+                        $page->save();
+                    }
+                    catch (\Exception $e) {
+                        $this->grav['debugger']->addMessage($e->getMessage());
+                        $this->grav->fireEvent('onFormValidationError', new Event([
+                            'form'    => $form,
+                            'message' => '<strong>ERROR:</strong> ' . $e->getMessage() ]));
+                        $event->stopPropagation();
+                        return;
+                    }
+                }
+        }
+    }
 }
 
 
