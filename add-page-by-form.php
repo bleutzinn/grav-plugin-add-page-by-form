8ce439e7a6258c940acf17817b01eed1c1391a01
bleutzinn <bleutzinn@rwgc.nl>
2017-02-12 19:35:35 +0100

    Added '@self' file upload and improved page creation and YAML handling.

--- add-page-by-form.php
+++ add-page-by-form.php
@@ -3,11 +3,12 @@ namespace Grav\Plugin;
 
 use Grav\Common\Grav;
 use Grav\Common\Plugin;
-use Grav\Common\Page\Page;
 use Grav\Common\Uri;
 use RocketTheme\Toolbox\Event\Event;
+use Grav\Common\Page\Page;
+use RocketTheme\Toolbox\File\File;
+use Symfony\Component\Yaml\Yaml;
 
-require_once "vendor/spyc.php";
 
 /**
  * Class AddPageByFormPlugin
@@ -15,6 +16,10 @@ require_once "vendor/spyc.php";
  */
 class AddPageByFormPlugin extends Plugin
 {
+
+    private $uploads = array();
+    private $moveSelfFiles = false;
+
     /**
      * @return array
      *
@@ -29,10 +34,114 @@ class AddPageByFormPlugin extends Plugin
     {
         return [
             'onPluginsInitialized' => ['onPluginsInitialized', 0],
-            'onFormProcessed' => ['onFormProcessed', 0]
+            'onFormProcessed' => ['onFormProcessed', 0],
+            'onFormValidationProcessed' => ['onFormValidationProcessed', 0]
+            // ALS onFormValidationProcessed WORDT AANGEROEPEN FAALT DE FILE MOVE VAN DE FORM PLUGIN
         ];
     }
 
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the events we are interested in
+        $this->enable([
+            'onPageInitialized' => ['onPageInitialized', 0],
+        ]);
+    }
+
+
+    public function onPageInitialized()
+    {
+        $assets = $this->grav['assets'];
+
+        // Add jQuery library
+        $assets->add('jquery', 101);
+
+        // Add SimpleMDE Markdown Editor
+        $assets->addCss('//cdn.jsdelivr.net/simplemde/latest/simplemde.min.css', 1);
+        $assets->addJs('//cdn.jsdelivr.net/simplemde/latest/simplemde.min.js', 1);
+
+        // Load inline Javascript code from configuration file
+        $assets->addInlineJs(file_get_contents('plugin://add-page-by-form/assets/js/simplemde_config.js'), 1);
+
+    }
+
+    public function onFormValidationProcessed(Event $event)
+    {
+        $grav = Grav::instance();
+        $config = $grav['config'];
+        $form = $event['form'];
+        $uri = $grav['uri']->url;
+        $session = $grav['session'];
+
+        // if files have been uploaded
+        // and
+        // destination is 'self'
+        // then prepare for moving the files to the new page
+        // else do nothing
+
+        //$this->uploads[] = [];
+        $this->moveSelfFiles = false;
+        $destination = $config->get('plugins.form.files.destination', '@self');
+        // Get queue from session
+        $queue = $session->getFlashObject('files-upload');
+        //dump('queue is '.serialize($queue));
+        $thisQueue = $queue[base64_encode($uri)];
+        $this->grav['log']->notice('This queue: '.serialize($thisQueue));
+        if ($thisQueue) {
+            //dump('This queue is filled');
+
+            $formdata = $form->toArray();
+            //dump($formdata);
+            $process = isset($formdata['process']) ? $formdata['process'] : [];
+
+            $formDef = $grav['page']->header()->form;
+            //dump($formDef);
+            //dump($formDef['fields']);
+            $fields = $formDef['fields'];
+            foreach ($fields as $array) {
+                //dump($array);
+                //dump(array_key_exists('destination', $array) );
+                if (array_key_exists('destination', $array)) {
+                    $destination = $array['destination'];
+                    //dump($destination);
+                    break;
+                }
+            }
+
+            if (is_array($process)) {
+                foreach ($process as $action => $data) {
+                    if (isset($action)) {
+                        $action = \key($data);
+                        if ($action === 'addpage') {
+                            $this->moveSelfFiles = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (($destination === '@self' || $destination === 'self@') && $this->moveSelfFiles) {
+            $this->grav['log']->notice('Will move uploaded files once form gets submitted');
+            // Save uploaded files properties to process in onFormProcessed() 
+            $this->uploads[] = $thisQueue;
+            //dump($thisQueue);
+        }
+        else {
+            // Restore queue in session again
+            $session->setFlashObject('files-upload', $queue);
+            $this->moveSelfFiles = false;
+            $this->grav['log']->notice('Will not move uploaded files; handing over back to Form plugin');
+        }
+    }
 
     public function onFormProcessed(Event $event)
     {
@@ -42,15 +151,15 @@ class AddPageByFormPlugin extends Plugin
     
         switch ($action) {
             case 'addpage':
-                //do what you want
                 if(isset($_POST)) {
                     // Get plugin config settings
-                    //$newPageRoute = $this->config->get('plugins.add-page-by-form.route');
-                    //$newPageTemplate = $this->config->get('plugins.add-page-by-form.template');
                     $dateFormat = $this->config->get('plugins.add-page-by-form.dateformat');
 
                     // Get all form fields
                     $formdata = $form->value()->toArray();
+
+                    $page = $this->grav['page'];
+
                     // Extract the content; if not present as a form value then fallback to frontmatter
                     $content = 'No content set';
                     if (isset($formdata['content']) ) {
@@ -61,27 +170,21 @@ class AddPageByFormPlugin extends Plugin
                             $content = $header->pagefrontmatter->content;
                         }
                     }
-
-                    //$this->grav['debugger']->addMessage('Content: ' . $content );
-
                     // Assemble the new page frontmatter from the pagefrontmatter block as set in
                     // the form page and the form field values. Field values override values set
                     // in the pagefrontmatter block
-                    $page = $this->grav['page'];
+                    $folder = $page->folder();
                     $header = $page->header();
+
                     $yaml_str = '';
                     if ( isset($header->pagefrontmatter) && is_array($header->pagefrontmatter) ) {
                         $pagefrontmatter = $header->pagefrontmatter;
                         $formdata = $form->value()->toArray();
                         if (isset($formdata)) {
-                            $pagefrontmatter = array_merge($pagefrontmatter, $form->value()->toArray());
+                            $pagefrontmatter = array_merge($pagefrontmatter, $formdata);
                         }
                         // Remove content from array
                         unset($pagefrontmatter['content']);
-                        // Convert array to a YAML formatted string
-                        $yaml_str = spyc_dump($pagefrontmatter);
-                        //dump($yaml_str);
-                        //$this->grav['debugger']->addMessage('The \'pagefrontmatter\' block is: ' . $yaml_str );
                     }
 
                     // Create s slug to be used as the page filename
@@ -106,49 +209,40 @@ class AddPageByFormPlugin extends Plugin
                     }
 
                     $newPageDir = $parent_page->path() . '/' . $slug;
-
                     // Assume this is the first submission of the page, so set $version to 1
                     $version = 0;
-
                     // Keep incrementing the page slug suffix to keep earlier versions / duplicates
                     while (file_exists($newPageDir)) {
                         $version += 1;
                         $newPageDir = $parent_page->path() . '/' . $slug . '-' . $version;
+
+                    }
+                    if ($version > 0) {
+                        $slug = $slug . '-' . $version;
                     }
 
                     // Add the page
                     try {
-                        // Create the directory
-                        $pageDir = mkdir($newPageDir, 0755, true);
-                        if (!$pageDir) {
-                            throw new \Exception('Unable to add page; can not create directory "' . $newPageDir . '"');
+                        /** @var Pages $pages */
+                        $pages = $this->grav['pages'];
+                        // Create page.
+                        $page = new Page;
+                        $language = trim(basename($parent_page->extension(), 'md'), '.') ?: null;
+                        if ($language != '') {
+                          $page->name('default.' . $language . '.md');
                         }
-                        // Create the page file
-                        $pageFile = fopen($newPageDir . '/default.md', "w");
-                        // test case $pageFile = false;
-                        if (!$pageFile) {
-                            throw new \Exception('Unable to add page; can not create file "default.md"');
+                        else {
+                            $page->name('default.md');
                         }
-                        // Include the page frontmatter
-                        $txt = "---\n";
-                        fwrite($pageFile, $txt);
-                        // Insert the timestamp
-                        $txt = "date: " . date($dateFormat) . "\n";
-                        fwrite($pageFile, $txt);
-                        // Insert all frontmatter
-                        if ($yaml_str != '') {
-                            $txt = $yaml_str;
-                            fwrite($pageFile, $txt);
-                        }
-                        $txt = "---\n";
-                        fwrite($pageFile, $txt);
-
-                        // Include the page content
-                        $txt = $content . "\n";
-                        fwrite($pageFile, $txt);
-
-                        // Close and save the file
-                        fclose($pageFile);
+                        $path = $parent_page->path() . DS . $slug. DS . $page->name();
+                        $page->filePath($path);
+                        $header = $pagefrontmatter;
+                        $page->header((object) $header);
+                        $page->frontmatter(Yaml::dump((array) $page->header(), 10, 2, false));
+                        $page->rawMarkdown((string) $content);
+                        $page->content((string) $content);
+                        $page->file()->markdown($page->rawMarkdown());
+                        $page->save();
                     }
                     catch (\Exception $e) {
                         $this->grav['debugger']->addMessage($e->getMessage());
@@ -158,42 +252,32 @@ class AddPageByFormPlugin extends Plugin
                         $event->stopPropagation();
                         return;
                     }
-    
+
+                    // Move uploaded files to the new page folder
+                    if ($this->moveSelfFiles) {
+                        $uploads = $this->uploads;
+                        //dump($uploads);
+                        foreach ($uploads as $key => $upload) {
+                            foreach ($upload as $key => $files) {
+                                foreach ($files as $destination => $file) {
+                                    foreach ($file as $properties => $value) {
+                                        $this->grav['log']->notice('properties: ' . $properties . '; value: ' . $value);
+                                    }
+                                    $destination = $file['path']; 
+                                    $tmp_name = $file['tmp_name'];
+                                    if (strpos($destination, 'user/pages/' . $folder) !== false) {
+                                        $filename = substr($destination, strrpos($destination, DS) + 1);
+                                        $originFilePath = ROOT_DIR . $destination;
+                                        $destinationFilePath = $parent_page->path() . DS . $slug . DS . $filename;
+                                        rename($tmp_name, $destinationFilePath);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
         }
     }
-
-    /**
-     * Initialize the plugin
-     */
-    public function onPluginsInitialized()
-    {
-        // Don't proceed if we are in the admin plugin
-        if ($this->isAdmin()) {
-            return;
-        }
-
-        // Enable the events we are interested in
-        $this->enable([
-            'onPageInitialized' => ['onPageInitialized', 0]
-        ]);
-    }
-
-    public function onPageInitialized()
-    {
-        $assets = $this->grav['assets'];
-
-        // Add jQuery library
-        $assets->add('jquery', 101);
-
-        // Add SimpleMDE Markdown Editor
-        $assets->addCss('//cdn.jsdelivr.net/simplemde/latest/simplemde.min.css', 1);
-        $assets->addJs('//cdn.jsdelivr.net/simplemde/latest/simplemde.min.js', 1);
-
-        // Load inline Javascript code from configuration file
-        $assets->addInlineJs(file_get_contents('plugin://add-page-by-form/assets/js/simplemde_config.js'), 1);
-    }
-
 }
 
 
