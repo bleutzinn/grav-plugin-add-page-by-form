b906b281e4d06b8d6c3ed31db4cf5a1f40c954b3
pd-giz-dave <pd.giz.dave@gmail.com>
2020-01-25 11:20:58 +0000

    Initial commit

--- add-page-by-form.php
+++ add-page-by-form.php
@@ -117,7 +117,7 @@ class AddPageByFormPlugin extends Plugin
      * @param string $route
      * @param boolean $create
      *
-     * @return string $path
+     * @return object|null [route, path]
      *
      * Crawl a route, using modular page folder names as a fallback and
      * optionally creating non existing folders along the way
@@ -539,7 +539,7 @@ class AddPageByFormPlugin extends Plugin
                         $new_page->save();
 
                         // Add page to Pages object with routing info
-                        $pages->addPage($new_page, $path);
+                        $pages->addPage($new_page, $this->new_page_route); //23/01/20 DCN: Hack-->$path);
 
                         // Support below and above version 1.6
                         if (version_compare(GRAV_VERSION, '1.6', '<')) {
