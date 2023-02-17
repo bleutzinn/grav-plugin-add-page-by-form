5c3e1f03ce91710e47b335a6fd235a3e516945fe
Ron Wardenier <bleutzinn@rwgc.nl>
2023-02-17 16:52:00 +0100

    Added composer to project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'grav-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'bleutzinn/grav-plugin-add-page-by-form',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bleutzinn/grav-plugin-add-page-by-form' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'grav-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
